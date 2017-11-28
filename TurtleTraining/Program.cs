50c48199b9e66b67caef1f36965e09b93abf9c81
Pavel340 <zinovev-pasha@mail.ru>
2017-11-28 12:02:39 +0300

    Начало работы с игрой Turtle

---
+++ TurtleTraining/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 9;
+            int i = 0;
+            Turtle.Move(100);
+            while (i < 2)
+            {
+                Turtle.Turn(45);
+                Turtle.Move(100);
+                Turtle.Turn(90);
+                Turtle.Move(100);
+                Turtle.Turn(45);
+                Turtle.Move(100);
+                i++;
+            }
+            
+        }
+    }
+}
